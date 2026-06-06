d8948a85cc891e1768b6d9157a84c748eedef241
maricruz-vs <128513656+maricruz-vs@users.noreply.github.com>
2026-06-06 02:46:33 +0000

    conway_logica del trabajo en clase

--- Proyecto 1/conway_logica.py
+++ Proyecto 1/conway_logica.py
@@ -1,14 +1,22 @@
 from random import randint
-
 def generar_matriz(filas, columnas):
     """Función que retorna una matriz de las dimensiones
     especificadas con valores enteros aleatorios de 0 o 1"""
-    return [[randint(0, 1) for c in range(columnas)] for f in range(filas)]    
+    return [[randint(0, 1) for c in range(columnas)] for f in range(filas)]
 
 def obtener_vecinos(M, f, c):
     """Función que retorna una lista con los estados de
     los 8 vecinos de la célula en la posición f, c de M."""
-    return [0 for e in range(8)]
+    vecinos = []
+    for df in [-1, 0, 1]:
+        for dc in [-1, 0, 1]:
+            if df==0 and dc==0:
+                continue
+            nf = (f+df)%len(M)
+            nc = (c+dc)%len(M[0])
+            vecinos.append(M[nf][nc])
+
+    return vecinos
 
 def transicion_celula(estado, vecinos):
     """Retorna el nuevo estado de la célula de acuerdo
@@ -17,10 +25,25 @@ def transicion_celula(estado, vecinos):
     Si estado == 1 y tiene menos de 2 vecinos vivos --> muere
     Si estado == 1 y tiene más de 3 vecinos vivos --> muere
     Cualquier otra combinación, el estado sigue igual."""
-    return 0
+    if estado==0 and vecinos.count(1) == 3:
+        return 1
+    elif estado == 1 and vecinos.count(1)<2:
+        return 0
+    elif estado == 1 and vecinos.count(1)>3:
+        return 0
+    else:
+        return estado
 
 def transicion(M):
     """Toma a la matriz completa y le aplica la función de
     transición a cada célula con su propio vecindario y deja
     el resultado en una matriz nueva."""
-    return M
+    nueva_matriz = []
+    for f in range(len(M)):
+        fila = []
+        for c in range(len(M[0])):
+            vecinos = obtener_vecinos(M, f, c)
+            nuevo_estado = transicion_celula(M[f][c], vecinos)
+            fila.append(nuevo_estado)
+        nueva_matriz.append(fila)
+    return nueva_matriz
